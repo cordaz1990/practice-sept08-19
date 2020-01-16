15ba31c0bee539a049a199067060de58d9c14003
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 22:54:47 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,16 @@
-print('hello')
-print('world')
-print('hello')
+#conditional Expressions
+if x > 0:
+  y = math.log(x)
+else:
+  y = float('nan')
+  
+y = math.log(x) if x > 0 else float('nan')
+
+def factor(n):
+    if n == 0:
+       return 1
+    else:
+       return n * factorial(n - 1)
+      
+def factorial(n)
+     return 1 if n == 0 else n * factorial(n - 1)
